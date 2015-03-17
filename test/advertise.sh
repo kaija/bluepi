9e6e98fb7f03fbecb1aee6825f37820f80be4a36
kaija <kaija.chang@gmail.com>
2015-03-17 16:05:52 +0800

    add bluez hcidump source code

---
+++ test/advertise.sh
@@ -0,0 +1,11 @@
+hciconfig hci0 up
+hciconfig hci0 leadv
+#hcitool -i hci0 cmd 0x08 0x0008 1e
+#  1e length
+#  
+
+hcitool -i hci0 cmd 0x08 0x0008 1e 02 01 1a 1a ff 4c 00 02 15 e2 c5 6d b5 df fb 48 d2 b0 60 d0 f5 a7 10 96 e0 00 00 00 00 c5 00 00 00 00 00 00 00 00 00 00 00 00 00
+hcitool -i hci0 cmd 0x08 0x0006 A0 00 A0 00 03 00 00 00 00 00 00 00 00 07 00
+hcitool -i hci0 cmd 0x08 0x000a 01
+
+
